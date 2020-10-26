411d28782a2f96489abac041bd8a26f6a73f1f02
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-10-26 18:27:48 +0530

    Navbar completed

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Nav.css';
 
 const Nav = () => {
+    const [show, setShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                setShow(true);
+            } else {
+                setShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener('scroll');
+        };
+    }, []);
+
     return (
-        <div className='nav'>
+        <div className={`nav ${show && 'navBlock'}`}>
             <img
                 className='navLogo'
                 src='https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png'
