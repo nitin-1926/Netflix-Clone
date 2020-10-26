f5af9479d89acdc2055e15d1b90b70052aff00f5
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-10-26 21:32:54 +0530

    Updates

---
+++ precache-manifest.ce8fce566a99c4a0371d4c2cd6002253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910dbe454f657b85b9c5027c565b373f",
+    "url": "/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "7ba08c301216c092c7f0",
+    "url": "/Netflix-Clone/static/css/main.2e124c15.chunk.css"
+  },
+  {
+    "revision": "91a840c1cedb0c980dae",
+    "url": "/Netflix-Clone/static/js/2.ae7ef797.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/Netflix-Clone/static/js/2.ae7ef797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ba08c301216c092c7f0",
+    "url": "/Netflix-Clone/static/js/main.e11db80e.chunk.js"
+  },
+  {
+    "revision": "b2b8b24758047419b104",
+    "url": "/Netflix-Clone/static/js/runtime-main.f28e37b4.js"
+  }
+]);
